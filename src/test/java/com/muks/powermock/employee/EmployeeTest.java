5ccb1e18906debefa9740cf6d3fd756fcd6dc7c7
Mukthar  Ahmed M <mukthar.am@gmail.com>
2016-07-11 19:03:34 +0530

    init commit

---
+++ src/test/java/com/muks/powermock/employee/EmployeeTest.java
@@ -0,0 +1,42 @@
+package com.muks.powermock.employee;
+
+
+import com.muks.powermock.employee.employee.EmployeeController;
+import com.muks.powermock.employee.employee.EmployeeService;
+import org.powermock.api.mockito.PowerMockito;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class EmployeeTest {
+
+    @Test
+    public void testMyFirst() {
+        System.out.println("# My first test.");
+        Assert.assertEquals(true, true);
+    }
+
+    @Test
+    public void testMySecond() {
+        System.out.println("# My Second test.");
+        Assert.assertEquals(true, true);
+    }
+
+    @Test(expectedExceptions = UnsupportedOperationException.class)
+    public void shouldGetCountOfEmployeesTest() {
+        EmployeeController employeeController =new EmployeeController(new EmployeeService());
+        Assert.assertEquals(10,employeeController.getProjectedEmployeeCount());
+    }
+
+    @Test
+    public void firstMockTest() {
+        // create mock
+        EmployeeService mock = PowerMockito.mock(EmployeeService.class);
+        PowerMockito.when(mock.getEmployeeCount()).thenReturn(8);
+
+        EmployeeController employeeController = new EmployeeController(mock);
+        Assert.assertEquals(16, employeeController.getProjectedEmployeeCount());
+    }
+}
